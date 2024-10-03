1653e411d51914957da0e4da65a4ba62c88aecbc
qscbm187531 <1309635304@qq.com>
2024-10-03 11:35:28 +0800

    Update build.gradle.kts

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -8,29 +8,6 @@ dependencies {
     implementation("com.willfp:ModelEngineBridge:1.0.0")
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            version = project.version.toString()
-            artifactId = rootProject.name
-
-            artifact(rootProject.tasks.shadowJar.get().archiveFile)
-        }
-    }
-
-    repositories {
-        maven {
-            name = "auxilor"
-            url = uri("https://repo.auxilor.io/repository/maven-releases/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
-    }
-}
-
 tasks {
     build {
         dependsOn(publishToMavenLocal)
